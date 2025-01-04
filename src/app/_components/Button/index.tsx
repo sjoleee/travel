d86306d17b0878ad13bc874cf9896d610fbf880a
sjoleee <cent7425@gmail.com>
2025-01-04 15:55:34 +0900

    feat: 버튼 active scale 수정

--- src/app/_components/Button/index.tsx
+++ src/app/_components/Button/index.tsx
@@ -5,7 +5,7 @@ import { Slot } from "@radix-ui/react-slot";
 import cn from "@/utils/cn";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-[colors,transform] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring active:scale-95 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-[colors,transform] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring active:scale-[0.97] disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
   {
     variants: {
       variant: {
