800a217df5f16402d61bd7936eb1385dba42935b
sjoleee <cent7425@gmail.com>
2025-01-04 16:11:35 +0900

    feat: BottomCTA가 컨텐츠에 붙게 padding 수정

--- src/app/_components/CTA/BottomCTA.tsx
+++ src/app/_components/CTA/BottomCTA.tsx
@@ -4,7 +4,7 @@ import CTA from ".";
 
 const BottomCTA = (props: ComponentProps<typeof CTA>) => {
   return (
-    <div className="sticky bottom-0 mt-auto w-full max-w-screen-sm bg-white p-4">
+    <div className="sticky bottom-0 mt-auto w-full max-w-screen-sm bg-white p-4 pt-0">
       <CTA {...props} />
     </div>
   );
